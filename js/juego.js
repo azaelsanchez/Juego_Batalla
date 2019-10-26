de0e6e008da16107461aa6b45c11ec81041a5938
azaelsanchez <cem20619@gmail.com>
2019-10-26 17:33:29 +0200

    Juego batalla
    Juego de batalla por ahora con combate 1vs1 con rival random

--- js/juego.js
+++ js/juego.js
@@ -7,22 +7,22 @@ let juego = {
     resetPlayer: function(p1clase) {
         switch (p1clase) {
             case "mago":
-                player = new Player (p1clase, 125,250, 175, 105, 75);
+                player1 = new Player (p1clase, 125,250, 175, 105, 75);
                 break;
             case "guerrero":
-                player = new Player(p1clase, 190,0, 250, 100, 55);
+                player1 = new Player(p1clase, 190,0, 250, 100, 55);
                 break;
             case "monje":
-            player = new Player(p1clase, 170,125, 190, 135, 100);
+                player1 = new Player(p1clase, 170,125, 190, 135, 100);
             break;
             case "ninja":
-                player = new Player(p1clase, 140,75, 140, 190, 150);
+                player1 = new Player(p1clase, 140,75, 140, 190, 150);
                 break;
         }
         let getInterface = document.querySelector(".interface");
         getInterface.innerHTML = '<img src="img/' + p1clase.toLowerCase() +  '.jpg" class="img-avatar"><div><h3>' + p1clase 
-        + '</h3><p class=".vida-player1">Vida: ' + player.vida + '</p><p>Mana: ' + player.mana + '</p><p>Fuerza: ' + player.fuerza + '</p><p>Agilidad: ' 
-        + player.agilidad + '</p><p>Velocidad: ' + player.velocidad + '</p></div>';
+        + '</h3><p class=".vida-player1">Vida: ' + player1.vida + '</p><p>Mana: ' + player1.mana + '</p><p>Fuerza: ' + player1.fuerza + '</p><p>Agilidad: ' 
+        + player1.agilidad + '</p><p>Velocidad: ' + player1.velocidad + '</p></div>';
     }, 
 
     antesBatalla: function(){
@@ -39,37 +39,90 @@ let juego = {
         let getMovimientos = document.querySelector(".movimientos");
         let getPlayer2 = document.querySelector(".player2");
     
-        let player21 = new Player2("Mago", 125,250, 175, 105, 75);
-        let player22 = new Player2("Guerrero", 190,0, 250, 100, 55);
-        let player23 = new Player2("Monje", 170,125, 190, 135, 100);
-        let player24 = new Player2("Ninja", 140,75, 140, 190, 150);
-        let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(4));
-    
-       
-        switch (chooseRandomEnemy) {
-            case 0:
-                Player2 = player21;
-                break;
-            case 1:
-            Player2 = player22;
-            break;
-            case 2:
-                Player2 = player23;
-                break;
-            case 3:
-            Player2 = player24;
-            break;
-            
-            default:
-                break;
-        }
+        let player21 = new Player("Mago", 125,250, 175, 105, 75);
+        let player22 = new Player("Guerrero", 190,0, 250, 100, 55);
+        let player23 = new Player("Monje", 170,125, 190, 135, 100);
+        let player24 = new Player("Ninja", 140,75, 140, 190, 150);
+        let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(3));
+        let playerArray = [
+            player21,
+            player22,
+            player23,
+            player24,
+        ];
+        player2 = playerArray[chooseRandomEnemy];
+        
         getHeader.innerHTML = '<p>Elige</p>'
-        getMovimientos.innerHTML = '<br><a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Ataca</a>';
-        getPlayer2.innerHTML = '<img src="img/' + Player2.p2clase.toLowerCase() +  '.jpg" class="img-avatar-p2"><div><h3>' + Player2.p2clase 
-        + '</h3><p class="vida-player2">Vida: ' + Player2.vida + '</p><p>Mana: ' + Player2.mana + '</p><p>Fuerza: ' + Player2.fuerza + '</p><p>Agilidad: ' 
-        + Player2.agilidad + '</p><p>Velocidad: ' + Player2.velocidad + '</p></div>';
+        getMovimientos.innerHTML = '<br><a href="#" class="btn-prefight" onclick="juego.hit()">Ataca</a>';
+        getPlayer2.innerHTML = '<img src="img/' + player2.clase.toLowerCase() +  '.jpg" class="img-avatar-p2"><div><h3>' + player2.clase 
+        + '</h3><p class="vida-player2">Vida: ' + player2.vida + '</p><p>Mana: ' + player2.mana + '</p><p>Fuerza: ' + player2.fuerza + '</p><p>Agilidad: ' 
+        + player2.agilidad + '</p><p>Velocidad: ' + player2.velocidad + '</p></div>';
 
+
+
+    },
+    hit: function(){
+        let Player1Speed = player1.velocidad;
+        let Player2Speed = player2.velocidad; 
+         // CAmbiar vida segun los golpes 
+    let getPlayer1Vida = document.querySelector(".vida-player1");
+    let getPlayer2Vida = document.querySelector(".vida-player2");
+    // Iniciar los ataques
+
+    if (Player1Speed >= Player2Speed ){
+        let playerAttackValues = player1.playerAttack();
+        let totalDamage = playerAttackValues [0] * playerAttackValues [1];
+        player2.vida = player2.vida - totalDamage;
+        alert("El Player 1 golpeo " +playerAttackValues [0] +" de daño "+ playerAttackValues [1] + " veces.");
+        if (player2.vida <= 0){
+            alert("Gano el player 1!!!");
+            getPlayer1Vida.innerHTML = 'Vida: '+ player1.vida;
+            getPlayer2Vida.innerHTML = 'Vida: 0';
+
+        }else{
+            getPlayer2Vida.innerHTML = 'Vida: '+ player2.vida;
+            let player2AttackValues = player2.playerAttack();
+            let totalDamage = player2AttackValues [0] * player2AttackValues [1];
+            player1.vida = player1.vida - totalDamage;
+        alert("El Player 2 golpeo " +player2AttackValues [0] +" de daño "+ player2AttackValues [1] + " veces.");
+        if (player1.vida <= 0){
+            alert("Gano el player 2!!!");
+            getPlayer1Vida.innerHTML = 'Vida: 0';
+            getPlayer2Vida.innerHTML = 'Vida: '+ player2.vida;
+        }else{
+            getPlayer1Vida.innerHTML = 'Vida: '+ player.vida;
+
+        }
+     }
 
     }
+    if (Player2Speed >= Player1Speed ){
+        let player2AttackValues = player2.playerAttack();
+        let totalDamage = player2AttackValues [0] * player2AttackValues [1];
+        player1.vida = player1.vida - totalDamage;
+        alert("El PLayer 1 golpeo " +player2AttackValues [0] +" de daño "+ player2AttackValues [1] + " veces.");
+        if (player1.vida <= 0){
+            alert("Gano el player 2!!!");
+            getPlayer2Vida.innerHTML = 'Vida: '+ player2.vida;
+            getPlayer1Vida.innerHTML = 'Vida: 0';
+
+        }else{
+            getPlayerVida.innerHTML = 'Vida: '+ player1.vida;
+            let playerAttackValues = player1.playerAttack();
+            let totalDamage = playerAttackValues [0] * playerAttackValues [1];
+            player1.vida = player1.vida - totalDamage;
+        alert("El PLayer 1 golpeo " +playerAttackValues [0] +" de daño "+ playerAttackValues [1] + " veces.");
+        if (Player2.vida <= 0){
+            alert("Gano el player 1!!!");
+            getPlayer2Vida.innerHTML = 'Vida: 0';
+            getPlayer1Vida.innerHTML = 'Vida: '+ player1.vida;
+        }else{
+            getPlayer2Vida.innerHTML = 'Vida: '+ player2.vida;
+
+    }
+   }
+  }
+}
     
+
 }
--- js/player.js
+++ js/player.js
@@ -1,109 +1,28 @@
 let player1;
 
-function Player (p1clase, vida, mana, fuerza, agilidad, velocidad ) {
-    this.p1clase = p1clase;
-    this.vida = vida;
-    this.mana = mana;
-    this.fuerza = fuerza;
-    this.agilidad = agilidad;
-    this.velocidad = velocidad;
-}
-
-let PlayerMoves = {
-    calcAttack: function (getPlayer1Speed,getPlayer2Speed) {
-        // Quien ataca antes?
-       this.getPlayer1Speed = getPlayer1Speed;
-       this.getPlayer2Speed = getPlayer2Speed;
-    },
-         //Player1 ataques
-     playerAttack : function(){
+class Player {
+    constructor(clase, vida, mana, fuerza, agilidad, velocidad) {
+        this.clase = clase;
+        this.vida = vida;
+        this.mana = mana;
+        this.fuerza = fuerza;
+        this.agilidad = agilidad;
+        this.velocidad = velocidad;
+    }
+    playerAttack(){
         let calcBaseDamage;
-        if(Player.mana > 0){
-            calcBaseDamage = Player.fuerza * Player.mana / 1000;
+        if(this.mana > 0){
+            calcBaseDamage = this.fuerza * this.mana / 1000;
         }else{
-            calcBaseDamage = Player.fuerza * Player.agilidad / 1000;
+            calcBaseDamage = this.fuerza * this.agilidad / 1000;
         }
         let offSetDamage = Math.floor(Math.random() * Math.floor(10));
         let calcOutputDamage = calcBaseDamage + offSetDamage;
         // Numero de golpes
-        let numberOfHits = Math.floor(Math.random() * Math.floor(Player.agilidad / 10) / 2) + 1;
+        let numberOfHits = Math.floor(Math.random() * Math.floor(this.agilidad / 10) / 2) + 1;
         let atackValues = [calcOutputDamage,numberOfHits];
         return atackValues;
-    },
-     //Player2 ataques
-     player2Attack : function(){
-        let calcBaseDamage;
-        if(Player2.mana > 0){
-            calcBaseDamage = Player2.fuerza * Player2.mana / 1000;
-        }else{
-            calcBaseDamage = Player2.fuerza * Player2.agilidad / 1000;
-        }
-        let offSetDamage = Math.floor(Math.random() * Math.floor(10));
-        let calcOutputDamage = calcBaseDamage + offSetDamage;
-        // Numero de golpes
-        let numberOfHits = Math.floor(Math.random() * Math.floor(Player2.agilidad / 10) / 2) + 1;
-        let atackValues = [calcOutputDamage,numberOfHits];
-        return atackValues;
-    },
-    // CAmbiar vida segun los golpes 
-     getPlayer1Vida = document.querySelector(".vida-player1");
-     getPlayer2Vida = document.querySelector(".vida-player2");
-    // Iniciar los ataques
-   }
-    if (getPlayer1Speed >= getPlayer2Speed ){
-        let playerAttackValues = playerAttack();
-        let totalDamage = playerAttackValues [0] * playerAttackValues [1];
-        Player2.vida = Player2.vida - totalDamage;
-        alert("El PLayer 1 golpeo " +playerAttackValues [0] +" de daño "+ playerAttackValues [1] + " veces.");
-        if (Player2.vida <= 0){
-            alert("Gano el player 1!!!");
-            getPlayer1Vida.innerHTML = 'Vida: '+ Player.vida;
-            getPlayer2Vida.innerHTML = 'Vida: 0';
-
-        }else{
-            getPlayer2Vida.innerHTML = 'Vida: '+ Player2.vida;
-            let player2AttackValues = player2Attack();
-            let totalDamage = player2AttackValues [0] * player2AttackValues [1];
-        Player.vida = Player.vida - totalDamage;
-        alert("El PLayer 2 golpeo " +player2AttackValues [0] +" de daño "+ player2AttackValues [1] + " veces.");
-        if (Player.vida <= 0){
-            alert("Gano el player 2!!!");
-            getPlayer1Vida.innerHTML = 'Vida: 0';
-            getPlayer2Vida.innerHTML = 'Vida: '+ Player2.vida;
-        }else{
-            getPlayer1Vida.innerHTML = 'Vida: '+ Player.vida;
-
-        }
-     }
-
     }
-    if (getPlayer2Speed >= getPlayer1Speed ){
-        let player2AttackValues = player2Attack();
-        let totalDamage = playerAttack2Values [0] * playerAttack2Values [1];
-        Player.vida = Player.vida - totalDamage;
-        alert("El PLayer 1 golpeo " +player2AttackValues [0] +" de daño "+ player2AttackValues [1] + " veces.");
-        if (Player.vida <= 0){
-            alert("Gano el player 2!!!");
-            getPlayer2Vida.innerHTML = 'Vida: '+ Player2.vida;
-            getPlayer1Vida.innerHTML = 'Vida: 0';
-
-        }else{
-            getPlayerVida.innerHTML = 'Vida: '+ Player.vida;
-            let playerAttackValues = playerAttack();
-            let totalDamage = playerAttackValues [0] * playerAttackValues [1];
-        Player.vida = Player.vida - totalDamage;
-        alert("El PLayer 1 golpeo " +playerAttackValues [0] +" de daño "+ playerAttackValues [1] + " veces.");
-        if (Player2.vida <= 0){
-            alert("Gano el player 1!!!");
-            getPlayer2Vida.innerHTML = 'Vida: 0';
-            getPlayer1Vida.innerHTML = 'Vida: '+ Player.vida;
-        }else{
-            getPlayer2Vida.innerHTML = 'Vida: '+ Player2.vida;
-
-        }
-     }
-
-    }
-
- }
 }
+
+
